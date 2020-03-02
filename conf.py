2e6690743ff2eb59277b46e2bd99271ac5c1520d
dat9uy <46886826+dat9uy@users.noreply.github.com>
2020-03-03 06:21:52 +0700

    Nikola auto commit.
    Nikola version: 8.0.4

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "dat9uy"  # (translatable)
 BLOG_TITLE = "Đoán toàn sai"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://dat9uy.github.io/posts"
+SITE_URL = "https://dat9uy.github.io/posts/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
